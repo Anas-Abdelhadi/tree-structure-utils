6a0d19951da76a0f38340034dcbf226184451a6c
Anas AbdelHadi <anas.a.abdelhadi@gmail.com>
2025-05-27 01:24:55 +0300

    structured inner children, allowed custom components

--- src/components/tree.vue
+++ src/components/tree.vue
@@ -1,23 +1,29 @@
 <template>
-  <Component :is="tag" class="tree-node">
+  <component :is="tag" class="tree-node" v-bind="resolveNodeAttrs(treeManager)">
     <slot :node="treeManager"></slot>
-    <Tree v-for="(child, index) in treeManager.children" :treeManager="child" :tag :key="index">
-      <template #default="slotProps">
-        <slot v-bind="slotProps" />
-      </template>
-    </Tree>
-  </Component>
+    <component :is="childrenWrapperTag" v-bind="childrenWrapperAttrs">
+      <Tree v-for="(child, index) in treeManager.children" :treeManager="child" :nodeAttrs :childrenWrapperTag :childrenWrapperAttrs :tag :key="index" v-bind="attrs">
+        <template #default="slotProps">
+          <slot v-bind="slotProps" />
+        </template>
+      </Tree>
+    </component>
+  </component>
 </template>
 
 <script setup lang="ts" generic="T">
 import { TreeManager } from '@/shared'
+import { useAttrs } from 'vue'
 
 const props = withDefaults(
   defineProps<{
+    childrenWrapperTag?: string
+    childrenWrapperAttrs?: Record<string, any>
     tag?: string
     treeManager: TreeManager<T>
+    nodeAttrs?: (manager: TreeManager<T>) => Record<string, any>
   }>(),
-  { tag: 'div' }
+  { tag: 'div', childrenWrapperTag: 'div', childrenWrapperAttrs: () => ({ id: 'children' }), nodeAttrs: () => () => ({}) }
 )
 
 // Infer the actual data type from TreeManager
@@ -26,8 +32,10 @@ type TreeDataType = typeof props.treeManager extends TreeManager<infer T> ? T : 
 defineSlots<{
   default(props: { node: TreeManager<TreeDataType> }): any
 }>()
-
-// const Node = defineAsyncComponent(() => import('../components/Tree.vue'))
+const attrs = useAttrs()
+const resolveNodeAttrs = (manager: TreeManager<T>) => {
+  return { ...attrs, ...(props.nodeAttrs?.(manager) || {}) }
+}
 </script>
 <style scoped>
 .tree-node {
